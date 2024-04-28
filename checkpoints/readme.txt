037ed396d1ad4dc1ce3e2d06c07f5ea2dff0c3cb
ValerioL29 <cheng2029@foxmail.com>
2024-04-28 22:24:52 +0800

    Down demo testing

--- checkpoints/readme.txt
+++ checkpoints/readme.txt
@@ -2,11 +2,15 @@
 
 - yolov9: https://drive.google.com/drive/folders/1CLPcgrVpCp3uINF4hYAJW-61Lvn92073?usp=share_link
 - yolov8: https://drive.google.com/drive/folders/1jGrs4mdm_GkUweMKwqKs_N1Up_sUkNsR?usp=share_link
-- CLRNet: https://drive.google.com/drive/folders/1UwtI4bxzk7xwW-kq8ca1qsINFHP8iLr5?usp=share_link
 
 # Then, place the checkpoints in the following directory:
 
 # checkpoints/yolov9
 # checkpoints/yolov8
-# checkpoints/CLRerNet
+
+# You can also download the model config files within the "cfg" folder,
+# and use it to load the model without transferring its pretrained weights.
+
+
+
 
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+from PIL import Image
 from rich import print
 from ultralytics import YOLO
+from ultralytics.engine.results import Results
 
-from hawkeye.utils import MODELS_DIR, DATA_DIR
+from hawkeye.utils import MODELS_DIR, DATA_DIR, OUTPUTS_DIR
 
 # Build a YOLOv9c model from scratch
 # model = YOLO('yolov9c.yaml')
 
-# Build a YOLOv9c model from pretrained weight
-model = YOLO(MODELS_DIR / "yolov9" / "yolov9e-seg.pt")
+# Build a YOLOv9c model from config file and transferring weights from a pre-trained model
+# This is the recommended way to load a model for training
+model = YOLO(MODELS_DIR / "cfg" / "yolov9" / "yolov9c.yaml") \
+    .load(MODELS_DIR / "yolov9" / "yolov9c.pt")
 
 # Display model information (optional)
 print(f"Model info: {model.info()}")
 
-# Train the model on the COCO8 example dataset for 100 epochs
-# results = model.train(data='coco8.yaml', epochs=100, imgsz=640)
-
 # Run inference with the YOLOv9c model on the 'bus.jpg' image
-# results = model(DATA_DIR / "demo" / "bus.jpg")
-# print(type(results))
-# print(type(results[0]))
+results: list[Results] = model(DATA_DIR / "demo" / "bus.jpg")
+
+for result in results:
+    # Save the result image
+    image_arr = result.plot()
+    image = Image.fromarray(image_arr[..., ::-1])  # RGB PIL image
+    image.save(OUTPUTS_DIR / "bus_yolov9c.jpg")
