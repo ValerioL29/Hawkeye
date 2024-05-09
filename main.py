42f3483cb06c832aa2b03c751e3fee16c044c580
ValerioL29 <cheng2029@foxmail.com>
2024-05-09 23:24:12 +0800

    Bump for lane label new

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ from hawkeye.utils import MODELS_DIR, DATA_DIR, OUTPUTS_DIR
 
 # Build a YOLOv9c model from config file and transferring weights from a pre-trained model
 # This is the recommended way to load a model for training
-model = YOLO(MODELS_DIR / "cfg" / "yolov9" / "yolov9c.yaml") \
-    .load(MODELS_DIR / "yolov9" / "yolov9c.pt")
+model = YOLO(MODELS_DIR / "cfg" / "yolov9" / "yolov9c-seg.yaml") \
+    .load(MODELS_DIR / "yolov9" / "yolov9c-seg.pt")
 
-results = model.train(data='test.yaml', epochs=1, imgsz=(1280, 720), device="gpu")
+print(f"Model info: {model.info()}")
 
+model_ret = model.train(
+    data=DATA_DIR / "lane_labels_trainval_new" / "lane.yaml",
+    epochs=1,
+    imgsz=(1280, 720),
+    amp=False,
+    device="mps"
+)
+
+# /Users/ken/miniconda3/envs/he/lib/python3.9/site-packages/ultralytics/data
 # Display model information (optional)
-# print(f"Model info: {model.info()}")
-print(f"Results: {results}")
-
-# # Run inference with the YOLOv9c model on the 'bus.jpg' image
-# results: list[Results] = model(DATA_DIR / "demo" / "bus.jpg")
-#
-# for result in results:
-#     # Save the result image
-#     image_arr = result.plot()
-#     image = Image.fromarray(image_arr[..., ::-1])  # RGB PIL image
-#     image.save(OUTPUTS_DIR / "bus_yolov9c.jpg")
+# p
